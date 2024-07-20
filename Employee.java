4079f28bba9a50f7a0d71b12bce8cbd288bf0e18
sahithi112003 <sahithi.karne04@gmail.com>
2024-07-20 16:22:38 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,21 @@
+package assignment;
+
+public class Employee {
+
+
+			
+			protected int EmpId=711;
+			protected String EmpName="sahithi";
+			protected int Empage=(int)35.5;
+			protected boolean ispermanent=true;
+			public static void main(String[] args) {
+				System.out.println("Successfully Started");
+			
+
+		}
+
+	
+
+	}
+
+
---
+++ Student.java
@@ -0,0 +1,21 @@
+package assignment;
+
+public class Student {
+
+	private int StuId=70;
+	private String name="Sahithi";
+	private String collegename="AVNT";
+	public static void main(String[] args) {
+		System.out.println("Successful");
+
+
+		    
+			
+			
+}
+
+	
+
+}
+
+
