e0cd96a1f2463aa6c327f9f4c6166e21c7c87065
sahithi112003 <sahithi.karne04@gmail.com>
2024-07-20 16:29:33 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,26 @@
+package com.anudip.learning;
+
+public class Rectangle {
+
+
+		double width;
+		double height;
+		color boxcolor;
+		enum color{
+			RED,GREEN,BLUE;
+
+		}
+		public static void main(String args[])
+		{
+			System.out.println(color.BLUE);
+		}
+
+
+	
+		
+
+	}
+
+
+
+
