2728ed4f2ff51857fa1dee0e178396693f287ccd
sahithi112003 <sahithi.karne04@gmail.com>
2024-07-21 13:25:24 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,17 @@
+package com.anudip.learning;
+
+public class Person {
+
+		
+		String name="sushmitha";
+		int Salary=45000;
+		int age=24;
+		public static void main(String[] args) {
+			System.out.println("Test successful");
+		
+
+	}
+
+}
+
+
